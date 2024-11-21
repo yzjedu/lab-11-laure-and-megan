02ed6b50b9013178d334309a590bc01cdb45ab84
LaurePatera <ampatera@loyola.edu>
2024-11-21 13:22:05 -0500

    code

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 
-def read_file_to_dictionary(filename):
+def read_file_to_dict(filename):
     # Open the file for reading
-    file_data = open(filename, 'r')
+
     morse_dict = {}
+    morse_data = open(filename, 'r')
 
     # Read each line, convert to integer, and append to the grades list
-    for line in file_data:
-        key, value = line.split('\t')
+    for line in morse_data:
+        items = line.split('  ')
+        key = items[1]
+        value = items[0]
         morse_dict[key] = value
-
-    # Close the file
-    file_data.close()
-
-    # Return the list of grades
-    return morse_dict
+    morse_data.close()
+    return morse_data
 
 def get_morse_file():
     morse_file = input('What is the file in morse code you would like to convert?')
     return morse_file
 
+def convert_file(morse_file, morse_dict):
+    morse_file_data = open(morse_file, 'r')
+
+
+
 def main():
-    filename = morsecode.txt
-    read_file_to_dictionary(filename)
-    get_morse_file()
+    print(read_file_to_dict('morsecode.txt'))
+
